40a4a4d19fe4f8b7a2292eb5784dd7bd7fb3d962
EzgiUOztuna <ezgiuygn@gmail.com>
2025-12-01 12:44:19 +0300

    checkout test done

--- test/specs/checkout.test.js
+++ test/specs/checkout.test.js
@@ -1,56 +1,33 @@
-const { Given, When, Then } = require("@wdio/cucumber-framework");
 const Checkout = require("../../features/pageobjects/checkout.page");
-const Login = require("../../features/pageobjects/login.page");
 
-Given("the user is in the cart", async () => {
-    await Checkout.open();
-});
+describe("Complete Purchase Successfully", () => {
+    it("User completes checkout with valid payment details", async () => {
+        await Checkout.open();
+        await Checkout.clickProceed();
+        await Checkout.login({
+            email: "test@mail.com",
+            password: "25112025*Epam"
+        });
 
-When("the user clicks on the 'Proceed to checkout'", async () => {
-    await Checkout.clickProceed();
-});
+        const userData = {
+            street: "Atatürk Caddesi",
+            city: "Istanbul",
+            state: "Marmara",
+            country: "Turkey",
+            postalCode: "34000"
+        }
+        await Checkout.address(userData);
+        await Checkout.submit3();
+        await Checkout.paymentSelection({ paymentOption: 'Kredi Kartı' });
 
-When("signs in again to proceed", async () => {
-    await Checkout.login({
-        email: "test@mail.com",
-        password: "25112025*Epam"
+        const cardData = {
+            creditCardNumber: '4111 1111 1111 1111',
+            expiration: '12/30',
+            CVV: '123',
+            cardHolderName: 'John Doe'
+        };
+        await Checkout.creditCardInfo(cardData);
+        await Checkout.submitPayment();
+        await Checkout.successMessage();
     });
 });
-
-When("enters the billing address details", async () => {
-    const userData = {
-        street: "Atatürk Caddesi",
-        city: "Istanbul",
-        state: "Marmara",
-        country: "Turkey",
-        postalCode: "34000"
-    }
-    await Checkout.address(userData);
-});
-
-
-When("clicks again 'Proceed to checkout' button", async () => {
-    await Checkout.submit3();
-});
-
-When("selects a payment option under 'Choose your payment method' section", async () => {
-    await Checkout.paymentSelection({ paymentOption: 'Kredi Kartı' });
-});
-
-When("enters valid information for payment", async () => {
-    const cardData = {
-        creditCardNumber: '4111 1111 1111 1111',
-        expiration: '12/30',
-        CVV: '123',
-        cardHolderName: 'John Doe'
-    };
-    await Checkout.creditCardInfo(cardData);
-});
-
-When("clicks on the 'Confirm' button", async () => {
-    await Checkout.submitPayment();
-});
-
-Then("a confirmation message should be displayed on the screen as a 'Payment was successful'", async () => {
-    await Checkout.successMessage();
-})
