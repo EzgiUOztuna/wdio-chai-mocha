96e54fc7a102e55ae3b8fc1c32fec57b1e718a04
EzgiUOztuna <ezgiuygn@gmail.com>
2025-12-01 12:35:04 +0300

    favorite products test done

--- test/specs/favorite-products.test.js
+++ test/specs/favorite-products.test.js
@@ -2,6 +2,20 @@ const { Given, When, Then } = require("@wdio/cucumber-framework");
 const FavoriteProducts = require("../pageobjects/favorite-products.page");
 const { expect, browser } = require("@wdio/globals");
 
+describe("Unsuccessful Adding Product to the Favorites", () => {
+    it("User fails to add a product to the Favorites list", async () => {
+        await FavoriteProducts.open();
+        await FavoriteProducts.clickFavoriteButton();
+        await FavoriteProducts.errorMessage('Unauthorized, can not add product to your favorite list.');
+
+        await browser.waitUntil(
+            async () => (browser.getUrl()) === "https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z"
+        );
+        const url = await browser.getUrl();
+        expect(url).to.equal('https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z');
+    })
+})
+
 Given("the user is on the product details page", async () => {
     await FavoriteProducts.open();
 });
