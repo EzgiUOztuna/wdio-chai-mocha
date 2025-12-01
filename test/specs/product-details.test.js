3cde56f375c835292a79064086d06999de4b6f96
EzgiUOztuna <ezgiuygn@gmail.com>
2025-12-01 12:23:57 +0300

    product details test done

--- test/specs/product-details.test.js
+++ test/specs/product-details.test.js
@@ -1,22 +1,11 @@
-const { Given, When, Then } = require("@wdio/cucumber-framework");
 const ProductDetails = require("../../features/pageobjects/product-details.page");
 
-Given("the user is on the product details page", async () => {
-    await ProductDetails.open();
-});
-
-When("the user clicks on the 'Add to cart' button", async () => {
-    await ProductDetails.clickCartButton();
-});
-
-Then("the product should be added to the basket", async () => {
-    await ProductDetails.successMessage();
-});
-
-Then("the basket counter should increase by one", async () => {
-    await ProductDetails.counter();
-});
-
-Then("confirmation message should be displayed saying 'Product added to shopping cart'", async () => {
-    await ProductDetails.successMessage();
+describe("Add Product to the Basket", () => {
+    it("User adds a product to the basket from product detail page", async () => {
+        await ProductDetails.open();
+        await ProductDetails.clickCartButton();
+        await ProductDetails.successMessage();
+        await ProductDetails.counter();
+        await ProductDetails.successMessage();
+    });
 });
