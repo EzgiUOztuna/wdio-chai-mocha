9156a0381c07be1fa6915878af84718860b59f68
EzgiUOztuna <ezgiuygn@gmail.com>
2025-12-01 12:36:51 +0300

    favorite products test done

--- test/specs/favorite-products.test.js
+++ test/specs/favorite-products.test.js
@@ -1,6 +1,5 @@
-const { Given, When, Then } = require("@wdio/cucumber-framework");
 const FavoriteProducts = require("../pageobjects/favorite-products.page");
-const { expect, browser } = require("@wdio/globals");
+const { expect } = require('chai');
 
 describe("Unsuccessful Adding Product to the Favorites", () => {
     it("User fails to add a product to the Favorites list", async () => {
@@ -13,21 +12,5 @@ describe("Unsuccessful Adding Product to the Favorites", () => {
         );
         const url = await browser.getUrl();
         expect(url).to.equal('https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z');
-    })
-})
-
-Given("the user is on the product details page", async () => {
-    await FavoriteProducts.open();
-});
-
-When("the user clicks on the 'Add to favorites' button", async () => {
-    await FavoriteProducts.clickFavoriteButton();
-});
-
-Then("an error message should be displayed saying {string}", async (message) => {
-    await FavoriteProducts.errorMessage(message);
-});
-
-Then("no changes should occur on the page", async () => {
-    await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z');
+    });
 });
