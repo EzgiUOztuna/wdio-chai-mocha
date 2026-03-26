8325f79fef6fc160ff42393679cd36703a7894cb
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 14:02:42 +0300

    Update favorite-products.page.js

--- src/pageobjects/favorite-products.page.js
+++ src/pageobjects/favorite-products.page.js
@@ -4,10 +4,6 @@ class FavoriteProducts extends Page {
     get addFavorites() { return $('[data-test="add-to-favorites"]'); }
     get errorToast() { return $('div[role="alert"][class*="toast-message"]'); }
 
-    async open() {
-        return super.open('product/01KB2FH1GEK977ZNZMDS0WXR9Z');
-    }
-
     async clickFavoriteButton() {
         await this.addFavorites.click();
     }
