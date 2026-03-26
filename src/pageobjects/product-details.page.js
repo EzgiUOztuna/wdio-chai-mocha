c194174fc0bfd478a6150dffa229b92505ba4d48
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 12:04:27 +0300

    Update product-details.page.js

--- src/pageobjects/product-details.page.js
+++ src/pageobjects/product-details.page.js
@@ -5,14 +5,14 @@ class ProductDetails extends Page {
     //get successToast() { return $('#toast-container'); }
 
     async goToHomePage() {
-        await browser.url('https://practicesoftwaretesting.com/');
+        await $('[data-test="nav-home"]').click();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/');
     }
 
     async clickProduct() {
-        const container = await $('app-overview');
-        const productLink = await container.$$('div.container a');
-        await productLink[2].click();
-
+        const product = await $('[data-test^="product-"]');
+        await product.waitForClickable({ timeout: 10000 });
+        await product.click();
     }
 
     async clickCartButton() {
@@ -26,11 +26,35 @@ class ProductDetails extends Page {
         await expect(successToast).toHaveText('Product added to shopping cart.');
     }
 
-    async counter() {
-        const basketCounter = await $('[data-test="cart-quantity"]');
-        const oldValue = parseInt(await basketCounter.getText());
-        const newValue = parseInt(await basketCounter.getText());
+    async getBasketCount() {
+        const elements = await $$('[data-test="cart-quantity"]');
+        if (elements.length === 0) {
+            return 0;
+        } else {
+            const basketCounter = elements[0];
+            await basketCounter.waitForDisplayed({ timeout: 5000 });
+            const count = await basketCounter.getText();
+            return parseInt(count);
+        }
+    }
 
+    async counter() {
+        const oldValue = await this.getBasketCount();
+        await this.clickCartButton();
+        await this.successMessage();
+
+        await browser.waitUntil(
+            async () => {
+                const newValue = await this.getBasketCount();
+                return newValue === oldValue + 1;
+            },
+            {
+                timeout: 10000,
+                timeoutMsg: 'Cart counter did not updated!'
+            }
+        );
+
+        const newValue = await this.getBasketCount();
         await expect(newValue).toEqual(oldValue + 1);
     }
 }
