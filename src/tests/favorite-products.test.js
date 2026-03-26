7f6a73fd53076b1aeca6245c6ceb82dcc6b7c4ec
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 14:02:59 +0300

    Update favorite-products.test.js

--- src/tests/favorite-products.test.js
+++ src/tests/favorite-products.test.js
@@ -1,16 +1,19 @@
 const FavoriteProducts = require("../pageobjects/favorite-products.page");
 const { expect } = require('chai');
+const { login } = require("../pageobjects/login.page");
+const loginPage = require("../pageobjects/login.page");
+const productDetailsPage = require("../pageobjects/product-details.page");
 
 describe("Unsuccessful Adding Product to the Favorites", () => {
+    beforeEach(async () => {
+        await loginPage.login("user@example.com", "Password123!");
+    });
+
     it("User fails to add a product to the Favorites list", async () => {
-        await FavoriteProducts.open();
+        await productDetailsPage.goToHomePage();
+        await productDetailsPage.clickProduct();
+
         await FavoriteProducts.clickFavoriteButton();
         await FavoriteProducts.errorMessage('Unauthorized, can not add product to your favorite list.');
-
-        await browser.waitUntil(
-            async () => (browser.getUrl()) === "https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z"
-        );
-        const url = await browser.getUrl();
-        expect(url).to.equal('https://practicesoftwaretesting.com/product/01KB2FH1GEK977ZNZMDS0WXR9Z');
     });
 });
