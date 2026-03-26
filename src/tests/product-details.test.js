36db85f6c59e40f998683d819326e355ad65e308
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 12:04:37 +0300

    Update product-details.test.js

--- src/tests/product-details.test.js
+++ src/tests/product-details.test.js
@@ -1,11 +1,14 @@
+const loginPage = require("../pageobjects/login.page");
 const productDetailsPage = require("../pageobjects/product-details.page");
 
 describe("Add Product to the Basket", () => {
+    beforeEach(async () => {
+        await loginPage.login("user@example.com", "Password123!");
+    });
+
     it("User adds a product to the basket from product detail page", async () => {
         await productDetailsPage.goToHomePage();
         await productDetailsPage.clickProduct();
-        await productDetailsPage.clickCartButton();
-        await productDetailsPage.successMessage();
         await productDetailsPage.counter();
     });
 });
