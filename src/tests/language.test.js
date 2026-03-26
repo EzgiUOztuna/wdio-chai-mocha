a07ba56bcd7c9dd663685ff2f98c85973db20e96
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 14:56:38 +0300

    Update language.test.js

--- src/tests/language.test.js
+++ src/tests/language.test.js
@@ -3,7 +3,7 @@ const loginPage = require("../pageobjects/login.page");
 
 describe("Switch from English to Turkish", () => {
     beforeEach(async () => {
-        await loginPage.login("user@example.com", "Password123!");
+        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
     });
 
     it("User changes the site language to Turkish", async () => {
