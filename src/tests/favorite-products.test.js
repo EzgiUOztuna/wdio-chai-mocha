10f8173a6cc43f458c6fa3edd86107da623ab1de
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 14:56:36 +0300

    Update favorite-products.test.js

--- src/tests/favorite-products.test.js
+++ src/tests/favorite-products.test.js
@@ -4,16 +4,16 @@ const { login } = require("../pageobjects/login.page");
 const loginPage = require("../pageobjects/login.page");
 const productDetailsPage = require("../pageobjects/product-details.page");
 
-describe("Unsuccessful Adding Product to the Favorites", () => {
+describe("Successful Adding Product to the Favorites", () => {
     beforeEach(async () => {
-        await loginPage.login("user@example.com", "Password123!");
+        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
     });
 
-    it("User fails to add a product to the Favorites list", async () => {
+    it("User successfully adds a product to the Favorites list", async () => {
         await productDetailsPage.goToHomePage();
         await productDetailsPage.clickProduct();
 
         await FavoriteProducts.clickFavoriteButton();
-        await FavoriteProducts.errorMessage('Unauthorized, can not add product to your favorite list.');
+        await FavoriteProducts.successMessage('Product added to your favorites list.');
     });
 });
