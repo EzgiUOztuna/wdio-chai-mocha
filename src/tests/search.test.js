e77e17b108da18badce8178ec00318bec71a95c1
Ezgi Uygun <ezgiuygun@192.168.1.18>
2026-03-26 14:03:04 +0300

    Update search.test.js

--- src/tests/search.test.js
+++ src/tests/search.test.js
@@ -1,8 +1,14 @@
+const loginPage = require("../pageobjects/login.page");
+const searchPage = require("../pageobjects/search.page");
 const Search = require("../pageobjects/search.page");
 
 describe("Find Product for Exact Name", () => {
+    beforeEach(async () => {
+        await loginPage.login("user@example.com", "Password123!");
+    });
+
     it("User searches for a product by its exact name", async () => {
-        await Search.open();
+        await searchPage.goToHomePage();
         await Search.clickSearchInput();
         await Search.searchForProduct('Hammer');
         await Search.submitSearchBtn();
