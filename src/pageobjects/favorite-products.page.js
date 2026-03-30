c930d49ef03879342e9f49290b512cbb73b05391
Ezgi Uygun <ezgiuygun@Ezgi-MacBook-Air-2.local>
2026-03-30 15:19:22 +0300

    Update favorite-products.page.js

--- src/pageobjects/favorite-products.page.js
+++ src/pageobjects/favorite-products.page.js
@@ -1,3 +1,4 @@
+const { expect } = require("@wdio/globals");
 const Page = require("./page");
 
 class FavoriteProducts extends Page {
@@ -11,6 +12,23 @@ class FavoriteProducts extends Page {
     async successMessage(expectedMessage) {
         await expect(this.successToast).toBeDisplayed();
         await expect(this.successToast).toHaveText(expectedMessage);
+    }
+
+    async removeFavorites() {
+        const fullnameButton = await $('[data-test="nav-menu"]');
+        await fullnameButton.click();
+
+        const dropdownMenu = await $('.dropdown-menu.show');
+        await expect(dropdownMenu).toBeDisplayed();
+
+        const favoriteButton = await $('[data-test="nav-my-favorites"]');
+        await favoriteButton.click();
+
+        await browser.url('https://practicesoftwaretesting.com/account/favorites');
+
+        const deleteButton = await $('[data-test="delete"]');
+        await deleteButton.click();
+        await expect(deleteButton).not.toExist();
     }
 }
 
