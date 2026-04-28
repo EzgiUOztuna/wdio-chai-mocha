3a1db77ef1c67159f59571f7cff70dea0307d32d
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 16:55:33 +0300

    Update language.test.js

--- src/tests/language.test.js
+++ src/tests/language.test.js
@@ -3,11 +3,13 @@ const loginPage = require("../pageobjects/login.page");
 
 describe("Switch from English to Turkish", () => {
     beforeEach(async () => {
-        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
+        await browser.loginAsCustomer3();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/account');
     });
 
     it("User changes the site language to Turkish", async () => {
         await Language.goToHomePage();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/');
         await Language.openDropdown();
         await Language.selectTr();
         await Language.verifyTurkish();
