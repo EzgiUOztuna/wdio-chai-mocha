8c8fbe172eacabcbcfca4bf7c4a4a05d319da91d
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 16:55:08 +0300

    Update checkout.page.js

--- src/pageobjects/checkout.page.js
+++ src/pageobjects/checkout.page.js
@@ -1,12 +1,7 @@
-const Page = require("./page");
+const Page = require("../core/page");
 const { expect, browser } = require("@wdio/globals");
 
 class Checkout extends Page {
-    get streetInput() { return $('[data-test="street"]'); }
-    get cityInput() { return $('[data-test="city"]'); }
-    get stateInput() { return $('[data-test="state"]'); }
-    get countrySelect() { return $('[data-test="country"]'); }
-    get postalCodeInput() { return $('[data-test="postal_code"]'); }
     get proceedBtn3() { return $('[data-test="proceed-3"]'); }
     get paymentSelect() { return $('[data-test="payment-method"]'); }
     get cardNumber() { return $('[data-test="credit_card_number"]'); }
@@ -14,7 +9,6 @@ class Checkout extends Page {
     get cvv() { return $('[data-test="cvv"]'); }
     get holderName() { return $('[data-test="card_holder_name"]'); }
     get finishPayment() { return $('button[data-test="finish"]'); }
-    get paymentSuccess() { return $('[data-test="payment-success-message"]'); }
 
     async goToCheckout() {
         const cartButton = await $('.nav-item a[data-test="nav-cart"]');
@@ -34,14 +28,6 @@ class Checkout extends Page {
         await proceedButton.click();
     }
 
-    async address({ street, city, state, country, postalCode }) {
-        await this.streetInput.setValue(street);
-        await this.cityInput.setValue(city);
-        await this.stateInput.setValue(state);
-        await this.countrySelect.setValue(country);
-        await this.postalCodeInput.setValue(postalCode);
-    }
-
     async submit3() {
         await this.proceedBtn3.click();
     }
@@ -59,11 +45,6 @@ class Checkout extends Page {
 
     async submitPayment() {
         await this.finishPayment.click();
-    }
-
-    async successMessage() {
-        await expect(this.paymentSuccess).toBeDisplayed();
-        await expect(this.paymentSuccess).toHaveText('Payment was successful');
     }
 }
 
