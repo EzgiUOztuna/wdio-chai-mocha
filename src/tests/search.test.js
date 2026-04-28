e64dc713757fab10874472f33b3233a98d31867e
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 16:55:39 +0300

    Update search.test.js

--- src/tests/search.test.js
+++ src/tests/search.test.js
@@ -4,11 +4,13 @@ const Search = require("../pageobjects/search.page");
 
 describe("Find Product for Exact Name", () => {
     beforeEach(async () => {
-        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
+        await browser.loginAsCustomer3();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/account');
     });
 
     it("User searches for a product by its exact name", async () => {
         await searchPage.goToHomePage();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/');
         await Search.clickSearchInput();
         await Search.searchForProduct('Hammer');
         await Search.submitSearchBtn();
