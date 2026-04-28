fbf3426b1ad0e74900e0f1ef389d628ac52e8e76
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 16:55:31 +0300

    Update favorite-products.test.js

--- src/tests/favorite-products.test.js
+++ src/tests/favorite-products.test.js
@@ -5,15 +5,18 @@ const productDetailsPage = require("../pageobjects/product-details.page");
 
 describe("Successful Adding Product to the Favorites", () => {
     beforeEach(async () => {
-        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
+        await browser.loginAsCustomer3();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/account');
     });
 
     it("User successfully adds a product to the Favorites list", async () => {
         await productDetailsPage.goToHomePage();
         await productDetailsPage.clickProduct();
-
         await FavoriteProducts.clickFavoriteButton();
-        await FavoriteProducts.successMessage('Product added to your favorites list.');
+
+        const successToast = await $('#toast-container');
+        await expect(successToast).toBeDisplayed();
+
         await FavoriteProducts.removeFavorites();
     });
 });
