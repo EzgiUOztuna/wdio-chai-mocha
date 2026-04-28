33cd345650d2fca3b83498681866975205df4098
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 16:55:37 +0300

    Update product-details.test.js

--- src/tests/product-details.test.js
+++ src/tests/product-details.test.js
@@ -3,12 +3,18 @@ const productDetailsPage = require("../pageobjects/product-details.page");
 
 describe("Add Product to the Basket", () => {
     beforeEach(async () => {
-        await loginPage.login("customer2@practicesoftwaretesting.com", "welcome01");
+        await browser.loginAsCustomer3();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/account');
     });
 
     it("User adds a product to the basket from product detail page", async () => {
         await productDetailsPage.goToHomePage();
+        await expect(browser).toHaveUrl('https://practicesoftwaretesting.com/');
         await productDetailsPage.clickProduct();
-        await productDetailsPage.counter();
+        const oldValue = await productDetailsPage.getBasketCount();
+        await productDetailsPage.clickCartButton();
+        await productDetailsPage.waitForCartCountToUpdate(oldValue);
+        const newValue = await productDetailsPage.getBasketCount();
+        await expect(newValue).toEqual(oldValue + 1);
     });
 });
