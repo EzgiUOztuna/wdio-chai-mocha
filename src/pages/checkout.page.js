e477373972827b079840d6aeebf6ac57e85629b0
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 18:08:18 +0300

    Create checkout.page.js

---
+++ src/pages/checkout.page.js
@@ -0,0 +1,51 @@
+const BasePage = require('../core/base-page');
+const { expect, browser } = require("@wdio/globals");
+
+class Checkout extends BasePage {
+    get proceedBtnThird() { return $('[data-test="proceed-3"]'); }
+    get paymentSelect() { return $('[data-test="payment-method"]'); }
+    get cardNumber() { return $('[data-test="credit_card_number"]'); }
+    get expirationDate() { return $('[data-test="expiration_date"]'); }
+    get cvv() { return $('[data-test="cvv"]'); }
+    get holderName() { return $('[data-test="card_holder_name"]'); }
+    get finishPayment() { return $('button[data-test="finish"]'); }
+
+    async goToCheckout() {
+        const cartButton = await $('.nav-item a[data-test="nav-cart"]');
+        await cartButton.click();
+        await browser.url("https://practicesoftwaretesting.com/checkout");
+    }
+
+    async clickProceed() {
+        const proceedButton = await $('button.btn.btn-success');
+        await proceedButton.waitForExist();
+        await proceedButton.click();
+    }
+
+    async clickProceedButtonSecond() {
+        const proceedButton = await $('button[data-test="proceed-2"]');
+        await proceedButton.waitForExist();
+        await proceedButton.click();
+    }
+
+    async submitButtonThird() {
+        await this.proceedBtnThird.click();
+    }
+
+    async paymentSelection({ paymentOption }) {
+        await this.paymentSelect.selectByAttribute('value', 'credit-card');
+    }
+
+    async creditCardInfo({ creditCardNumber, expiration, CVV, cardHolderName }) {
+        await this.cardNumber.setValue(creditCardNumber);
+        await this.expirationDate.setValue(expiration);
+        await this.cvv.setValue(CVV);
+        await this.holderName.setValue(cardHolderName);
+    }
+
+    async submitPayment() {
+        await this.finishPayment.click();
+    }
+}
+
+module.exports = new Checkout();
