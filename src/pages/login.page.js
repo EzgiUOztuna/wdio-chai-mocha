3725f9d18f49da77a9dce4f41c9d6605100ba356
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 18:08:24 +0300

    Create login.page.js

---
+++ src/pages/login.page.js
@@ -0,0 +1,42 @@
+const { expect } = require("@wdio/globals");
+const BasePage = require('../core/base-page');
+
+class Login extends BasePage {
+    get emailInput() { return $('[data-test="email"]'); }
+    get passwordInput() { return $('[data-test="password"]'); }
+    get submitButton() { return $('[data-test="login-submit"]'); }
+    get errorMessage() { return $('[data-test="login-error"]'); }
+
+    async open() {
+        return super.open('auth/login');
+    }
+
+    async fillData({ email, password }) {
+        await this.emailInput.setValue(email);
+        await this.passwordInput.setValue(password);
+    }
+
+    async login(email, password) {
+        await this.open();
+        await this.fillData({ email, password });
+        await this.submit();
+    }
+
+    async submit() {
+        await this.submitButton.click();
+    }
+
+    async isErrorDisplayed() {
+        return this.errorMessage.isDisplayed();
+    }
+
+    async getErrorText() {
+        return this.errorMessage.getText();
+    }
+
+    async getCurrentUrl() {
+        return browser.getUrl();
+    }
+}
+
+module.exports = new Login();
