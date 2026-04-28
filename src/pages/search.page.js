828caa20315227d32d9c7051830789a8ac1489d9
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-28 18:08:28 +0300

    Create search.page.js

---
+++ src/pages/search.page.js
@@ -0,0 +1,31 @@
+const { browser, expect } = require("@wdio/globals");
+const BasePage = require('../core/base-page');
+
+class Search extends BasePage {
+    get searchInput() { return $('[data-test="search-query"]'); }
+    get searchSubmit() { return $('[data-test="search-submit"]'); }
+    get searchResults() { return $$('h5[data-test="product-name"]'); }
+
+    async clickSearchInput() {
+        const input = await this.searchInput;
+        await browser.execute((el) => el.click(), input);
+    }
+
+    async searchForProduct(productName) {
+        const input = await this.searchInput;
+        await browser.execute((el) => el.focus(), input);
+        await input.setValue(productName);
+        await browser.keys('Enter');
+    }
+
+    async submitSearchBtn() {
+        await this.searchSubmit.click();
+    }
+
+    async verifyProductInResults(productName) {
+        const results = await this.searchResults;
+        await expect(results).toEqual(expect.arrayContaining([expect.stringContaining(productName)]));
+    }
+}
+
+module.exports = new Search();
