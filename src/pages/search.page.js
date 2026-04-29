7909cee8ef600f3afe3f45a802b194091a7d917b
Ezgi Uygun <ezgiuygun@192.168.1.24>
2026-04-29 15:53:03 +0300

    Update search.page.js

--- src/pages/search.page.js
+++ src/pages/search.page.js
@@ -4,7 +4,7 @@ const BasePage = require('../core/base-page');
 class Search extends BasePage {
     get searchInput() { return $('[data-test="search-query"]'); }
     get searchSubmit() { return $('[data-test="search-submit"]'); }
-    get searchResults() { return $$('h5[data-test="product-name"]'); }
+    get searchResults() { return $('[data-test="search_completed"]').$$('h5[data-test="product-name"]'); }
 
     async clickSearchInput() {
         const input = await this.searchInput;
@@ -24,10 +24,15 @@ class Search extends BasePage {
 
     async verifyProductInResults(productName) {
         await browser.waitUntil(async () => {
-            const results = await Promise.all(
-                await this.searchResults.map(async (el) => await el.getText())
-            );
-            return results.includes(productName);
+            const results = await this.searchResults.map(async (el) => await el.getText());
+            console.log('Search results:', results);
+
+            const validResults = results.filter(name =>
+                name.includes(productName)
+            ); //Since there is some mistakes in UI, we need to filter results to get only valid ones
+            console.log('Valid results:', validResults);
+
+            return validResults;
         }, {
             timeout: 5000,
             timeoutMsg: `Expected product "${productName}" not found in search results after 5 seconds`
